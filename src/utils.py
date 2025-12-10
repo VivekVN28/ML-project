9beb0131650a8b3c9ed358a5b70523af8f724b0a
VivekV-N <vivekvnambiar2004@gmail.com>
2025-12-10 09:39:12 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -5,11 +5,24 @@ from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 import dill
+from sklearn.metrics import r2_score
 def save_object(file_path,obj):
     try:
         dir_path=os.path.dirname(file_path)
         os.makedirs(dir_path,exist_ok=True)
         with open(file_path,'wb') as file_obj:
             dill.dump(obj,file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
+def evaluate_models(X_train,y_train,X_test,y_test,models):
+    try:
+        report={}
+        for i in range(len(models)):
+            model=list(models.values())[i]
+            model.fit(X_train,y_train)
+            y_pred=model.predict(X_test)
+            r2_square=r2_score(y_test,y_pred)
+            report[list(models.keys())[i]]=r2_square
+        return report
     except Exception as e:
         raise CustomException(e,sys)
